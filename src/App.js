4ec1cecb56b7ed9dceae3613b0abd03d1208843b
Pasha147 <pavel147223@gmail.com>
2023-12-21 19:35:37 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import React, { useState, useEffect } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { nav } from "./appdata";
+
+
 
 function App() {
+
+  const [curArticle, setCurArticle] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
+       
+      {nav.map((item) => {
+          return (
+            <Link
+              className={`navLink ${
+                item.id === curArticle ? "navBtn-active" : ""
+              }`}
+              key={item.id}
+              onClick={() => setCurArticle(item.id)}
+              to={item.route}
+            >
+              {item.name}
+            </Link>
+          );
+        })}
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/appdata.js
@@ -0,0 +1,53 @@
+
+//buttons in nav
+export const nav = [
+  { id: 0, 
+    name: "MAIN", 
+    artHeader: "Main", 
+    article: '', 
+    route: "/" },
+  {
+    id: 1,
+    name: "SKILLS",
+    artHeader: "Skills",
+    article: '',
+    route: "/skills",
+  },
+  {
+    id: 2,
+    name: "EDUCATION",
+    artHeader: "Education",
+    article: '',
+    route: "/education",
+  },
+  {
+    id: 3,
+    name: "WORK EXPERIENCE",
+    artHeader: "Work Experience",
+    article: '',
+    route: "/workexp",
+  },
+  {
+    id: 4,
+    name: "PROJECTS",
+    artHeader: "Projects",
+    article: '',
+    route: "/projects",
+  },
+
+  {
+    id: 5,
+    name: "ABOUT ME",
+    artHeader: "About me",
+    article: '',
+    route: "/aboutMe",
+  },
+  {
+    id: 6,
+    name: "CONTACTS",
+    artHeader: "Contacts",
+    article: '',
+    route: "/contactsArt",
+  },
+];
+//==================================
