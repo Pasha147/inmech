362e8e04f1c7a89f4ef64aae7db072e09e6a799e
Pasha147 <pavel147223@gmail.com>
2023-12-29 18:31:44 +0200

    FoundDocum has refactored

--- src/Components/FounDocum.js
+++ src/Components/FounDocum.js
@@ -2,21 +2,27 @@ const FounDocum = () => {
   return (
     <section className="section">
       <div className="container">
-        <h2>Установчі документи</h2>
         <article className="article">
-          <a href="./pdf/post.pdf" download="./pdf/post.pdf">
-            Постанова Президії АН України від 1992 року про заснування комітету
-          </a>
-        </article>
-        <article className="article">
-          <a href="./pdf/pol.pdf" download="./pdf/pol.pdf">
-            Положення про комітет від 1992 року, затверджене Президією академії
-          </a>
-        </article>
-        <article className="article">
-          <a href="./pdf/stat.pdf" download="./pdf/stat.pdf">
-            Статут комітету
-          </a>
+          <h2>Установчі документи</h2>
+          <ul>
+            <li>
+              <a href="./pdf/post.pdf" download="./pdf/post.pdf">
+                Постанова Президії АН України від 1992 року про заснування
+                комітету
+              </a>
+            </li>
+            <li>
+              <a href="./pdf/pol.pdf" download="./pdf/pol.pdf">
+                Положення про комітет від 1992 року, затверджене Президією
+                академії
+              </a>
+            </li>
+            <li>
+              <a href="./pdf/stat.pdf" download="./pdf/stat.pdf">
+                Статут комітету
+              </a>
+            </li>
+          </ul>
         </article>
       </div>
     </section>
