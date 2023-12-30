c087ce5a17586fe7262972a6c4a93b9acf1dc682
Pasha147 <pavel147223@gmail.com>
2023-12-30 11:17:33 +0200

    Styles for only one component (Error comp)

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,10 +1,11 @@
 
 import { Link } from 'react-router-dom';
+import classes from './Error.module.css';
 
 
 export default function Error ()  {
   return (
-    <section className='section'>
+    <section className={classes.section}>
       <h2>404</h2>
       <p>page not found</p>
       <Link to='/'>back home</Link>
