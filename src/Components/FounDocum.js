1707a38fb3583d164988c3e4264a34aa247ef05a
Pasha147 <pavel147223@gmail.com>
2024-02-01 17:39:49 +0200

    FoundDoucum refactored

--- src/Components/FounDocum.js
+++ src/Components/FounDocum.js
@@ -1,33 +1,46 @@
 const FounDocum = () => {
+  const fdLi = [
+    {
+      href: "./pdf/p26.pdf",
+      download: "./pdf/p26.pdf",
+      text: `Постанова Про затвердження Положення про 
+        Національний комітет України з теоретичної і прикладної 
+        механіки (24.01.2024).`,
+    },
+    {
+      href: "./pdf/post.pdf",
+      download: "./pdf/post.pdf",
+      text: `Постанова Президії АН України від 1992 року про 
+    заснування комітету.`,
+    },
+    {
+      href: "./pdf/pol.pdf",
+      download: "./pdf/pol.pdf",
+      text: `Положення про комітет від 1992 року, затверджене Президією
+    академії.`,
+    },
+    {
+      href: "./pdf/stat.pdf",
+      download: "./pdf/stat.pdf",
+      text: `Статут комітету.`,
+    },
+  ];
+
   return (
     <section className="section">
       <div className="container">
         <article className="article">
           <h2>Установчі документи</h2>
           <ul>
-            <li>
-              <a href="./pdf/p26.pdf" download="./pdf/p26.pdf">
-                {'Постанова Про затвердження Положення про Національний комітет України з теоретичної і прикладної механіки (24.01.2024)'}
-
-              </a>
-            </li>
-            <li>
-              <a href="./pdf/post.pdf" download="./pdf/post.pdf">
-                Постанова Президії АН України від 1992 року про заснування
-                комітету
-              </a>
-            </li>
-            <li>
-              <a href="./pdf/pol.pdf" download="./pdf/pol.pdf">
-                Положення про комітет від 1992 року, затверджене Президією
-                академії
-              </a>
-            </li>
-            <li>
-              <a href="./pdf/stat.pdf" download="./pdf/stat.pdf">
-                Статут комітету
-              </a>
-            </li>
+            {fdLi.map((item, num) => {
+              return (
+                <li key={`fdLi ${num}`}>
+                  <a href={item.href} download={item.download}>
+                    {item.text}
+                  </a>
+                </li>
+              );
+            })}
           </ul>
         </article>
       </div>
